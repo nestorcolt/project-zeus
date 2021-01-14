a379cb7e13f2d078d100067c197493acda7bc84e
nestor <nestorcolt@hotmail.com>
2021-01-14 20:25:17 +0100

    dynamo crud created

--- packages/dynamo/dynamo_manager.py
+++ packages/dynamo/dynamo_manager.py
@@ -1,3 +1,4 @@
+from string import ascii_lowercase
 from Cloud.packages import logger
 import boto3
 import json
@@ -24,20 +25,47 @@ def create_item(table_name, dictionary_item):
 
 def read_item(table_name, field, value):
     table = get_table_by_name(table_name)
-    resp = table.get_item(
-        Key={
-            field: value,
-        }
-    )
 
-    if 'Item' in resp:
-        return resp['Item']
+    try:
+        resp = table.get_item(
+            Key={
+                field: value,
+            }
+        )
+
+        if 'Item' in resp:
+            return resp['Item']
+
+    except Exception as e:
+        log.error(e)
 
     return None
 
 
-def update_item(table_name, dictionary_item):
-    pass
+def map_expression_attributes_values(dictionary_item):
+    expression_template = "set "
+    result_dict = {}
+
+    for index, (key, values) in enumerate(dictionary_item.items()):
+        key_name = f":{ascii_lowercase[index]}"
+        expression_template = expression_template + f"{key} = {key_name}, "
+        result_dict[key_name] = values
+
+    return expression_template[:-2], result_dict
+
+
+def update_item(table_name, primary_key, value, items):
+    expression_key, expression_values = map_expression_attributes_values(items)
+    table = get_table_by_name(table_name)
+
+    table.update_item(
+        Key={
+            primary_key: value,
+        },
+        UpdateExpression=expression_key,
+        ExpressionAttributeValues=expression_values,
+        ReturnValues="UPDATED_NEW"
+    )
 
 
 def delete_item(table_name, primary_key, value):
@@ -55,4 +83,5 @@ def delete_item(table_name, primary_key, value):
 
 ##############################################################################################
 if __name__ == '__main__':
-    read_item("Users", "user_id", 55)
+    data = {"search_blocks": False}
+    update_item("Users", "user_id", 55, data)
