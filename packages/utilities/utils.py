ddadbfd384c75fef68a98dc2bc9c5d0e3f55ef36
nestor <nestorcolt@hotmail.com>
2021-01-15 01:59:20 +0100

    remove utils

--- packages/utilities/utils.py
+++
@@ -1,6 +0,0 @@
-##############################################################################################
-def sns_get_value(value_pair):
-    result = list(value_pair.values())
-
-    if result:
-        return result[0]
