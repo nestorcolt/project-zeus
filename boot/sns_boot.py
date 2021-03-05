1b07332d6b70f5838ca7702edaebf3900a21d81a
nestor <nestorcolt@hotmail.com>
2021-03-05 13:47:13 +0100

    sqs removed

--- boot/sns_boot.py
+++ boot/sns_boot.py
@@ -9,40 +9,22 @@ def sns_boostrap():
     print("************************\nSNS\n************************")
     # Create sns to start the user search engine instance
     sns_manager.create_topic(name=constants.SE_START_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_START_DLQ,
-                                      topic_name=constants.SE_START_TOPIC)
-
     sns_manager.create_topic(name=constants.SE_STOP_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_STOP_DLQ,
-                                      topic_name=constants.SE_STOP_TOPIC)
-
     sns_manager.create_topic(name=constants.SE_AUTHENTICATE_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_AUTHENTICATE_DLQ,
-                                      topic_name=constants.SE_AUTHENTICATE_TOPIC)
 
     # Create sns to send a message when a block has been accepted
     sns_manager.create_topic(name=constants.SE_ACCEPTED_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_ACCEPTED_DLQ,
-                                      topic_name=constants.SE_ACCEPTED_TOPIC)
 
     # Create sns to send a message when a block has been accepted
     sns_manager.create_topic(name=constants.SE_SLEEP_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_SLEEP_DLQ,
-                                      topic_name=constants.SE_SLEEP_TOPIC)
 
     # Create sns to send LOGS
     sns_manager.create_topic(name=constants.SE_LOGS_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_LOGS_DLQ,
-                                      topic_name=constants.SE_LOGS_TOPIC)
 
     # Create sns to send LOGS
     sns_manager.create_topic(name=constants.SE_ERROR_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_ERROR_DLQ,
-                                      topic_name=constants.SE_ERROR_TOPIC)
 
     sns_manager.create_topic(name=constants.SE_OFFERS_TOPIC)
-    sns_manager.set_dead_letter_queue(queue_name=constants.SE_OFFERS_DLQ,
-                                      topic_name=constants.SE_OFFERS_TOPIC)
 
     time.sleep(2)
     print(f"SNS topics created!")
--- bootstraper.py
+++ bootstraper.py
@@ -1,14 +1,10 @@
-from Cloud.boot import ec2_boot, network_boot, sns_boot, sqs_boot, iam_boot
+from Cloud.boot import ec2_boot, network_boot, sns_boot, iam_boot
 from Cloud.packages.storage import buckets
 
 
 ##############################################################################################
 
 def bootstrap():
-    # Create SQS DEAD LETTERS
-    sqs_boot.sqs_boostrap()
-    print("##############################################################################################")
-
     # Create SNS topics
     sns_boot.sns_boostrap()
     print("##############################################################################################")
