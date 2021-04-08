883c4d1f9d1f78b1fd464af43144317c02907747
nestor <nestorcolt@hotmail.com>
2021-04-08 12:03:11 +0200

    update with headers

--- packages/controller/check_in_controller.py
+++ packages/controller/check_in_controller.py
@@ -1,5 +1,6 @@
 from packages.controller.user_controller import get_authorization_header, get_user_data, get_access_token
 from Cloud.packages.constants import constants
+from pprint import pprint
 import requests
 import time
 import json
@@ -23,7 +24,7 @@ def get_check_in_data(refresh_token, longitude, latitude):
                 "latitude": latitude,
                 "longitude": longitude,
                 "provider": "gps",
-                "time": time.time()
+                "time": int(time.time())
             }
         }
     })
@@ -43,14 +44,24 @@ def check_in_block(block_data):
     if user_data is None:
         return
 
-        # create the body to send as json in the POST request
-    check_in_data = get_check_in_data(user_data.get("refresh_token", ""),
+    refresh_token = user_data.get("refresh_token", None)
+
+    if refresh_token is None:
+        return
+
+    # create the body to send as json in the POST request
+    check_in_data = get_check_in_data(refresh_token,
                                       block_data.get("longitude"),
                                       block_data.get("latitude"))
 
     # Create post request
-    authorization_header = get_authorization_header(user_data.get("access_token", ""))
-    response = requests.post(constants.CHECK_IN_URL, json=check_in_data, headers=authorization_header, timeout=5)
+    authorization_header = get_authorization_header(get_access_token(refresh_token))
+    response = requests.post(constants.CHECK_IN_URL,
+                             json=json.dumps(check_in_data),
+                             headers=authorization_header,
+                             timeout=5)
+
+    pprint(response.content)
 
     if response.status_code == 200:
         message = "Block checked in successfully!"
@@ -62,5 +73,9 @@ def check_in_block(block_data):
     print(message, response)
     return {"response": response, "message": message}
 
+
 ##############################################################################################
-# check_in_block({"user_id": "5", "longitude": "", "latitude": ""})
+user_id = "5"
+# user_data = get_user_data({"user_id": user_id})
+# get_schedule(user_data["access_token"], user_data["refresh_token"])
+# check_in_block({"user_id": user_id, "longitude": "-80.323221", "latitude": "25.67329"})
--- packages/controller/user_controller.py
+++ packages/controller/user_controller.py
@@ -1,20 +1,41 @@
 from packages.dynamo import dynamo_manager
 from packages.constants import constants
+from pprint import pprint
+from uuid import uuid4
 import requests
+import time
 
 ##############################################################################################
 # GLOBALS
+DEVICE_MODEL = "SM-G977N"
+APP_VERSION = "3.51.3.3.0"
+
 ACCESS_HEADERS = {"app_name": "com.amazon.rabbit",
                   "source_token_type": "refresh_token",
                   "source_token": "refresh_token",
                   "requested_token_type": "access_token"}
 
+API_DEFAULT_HEADERS = {
+    "x-flex-instance-id": str(uuid4()),
+    "X-Flex-Client-Time": int(time.time()),
+    "Content-Type": "application/json",
+    "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 7.1.2; {DEVICE_MODEL} Build/N2G48C) RabbitAndroid/{APP_VERSION}",
+    "X-Amzn-RequestId": str(uuid4()),
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+}
+
 
 ##############################################################################################
 # Controller functions:
 
-def get_authorization_header(access_token):
+def get_authorization_header(access_token, default_headers=None):
     authorization_header = {"x-amz-access-token": access_token}
+
+    if default_headers:
+        authorization_header.update(default_headers)
+
+    pprint(authorization_header)
     return authorization_header
 
 
@@ -54,10 +75,7 @@ def get_schedule(access_token, refresh_token):
 
     response_json = response.json()
     schedule = response_json.get("scheduledAssignments", None)
+    pprint(schedule)
     return schedule
 
-
 ##############################################################################################
-if __name__ == '__main__':
-    user_data = get_user_data({"user_id": "5"})
-    get_schedule(user_data["access_token"], user_data["refresh_token"])
