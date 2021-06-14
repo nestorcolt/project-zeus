454a9d058106bc133c17ad07ba6739277c51cf42
nestor <nestorcolt@hotmail.com>
2021-06-14 18:26:41 +0200

    get bis users function

---
+++ packages/aurora/controller.py
@@ -0,0 +1,40 @@
+from Cloud.packages.security import secrets_manager
+from Cloud.packages.constants import constants
+import psycopg2
+import json
+
+
+##############################################################################################
+
+def connection_handler():
+    secret = json.loads(secrets_manager.get_secret(constants.AURORA_SECRET_NAME))
+
+    # connect to the db client
+    try:
+        conn = psycopg2.connect(dbname=secret["dbname"],
+                                user=secret["username"],
+                                password=secret["password"],
+                                host=secret["host"])
+
+        return conn
+
+    except Exception as e:
+        print(e)
+
+    return
+
+
+def get_bis_users():
+    conn = connection_handler()
+    cur = conn.cursor()
+
+    cur.execute('SELECT * FROM public."Users"')
+    result = cur.fetchall()
+    conn.commit()
+    conn.close()
+
+    # a tuple is returned, this will get the item in index 1 which is the user email
+    user_list = [itm[1] for itm in result]
+    return user_list
+
+##############################################################################################
