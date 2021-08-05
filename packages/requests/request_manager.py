b2f0d047dc2ccdef64a0d1d3a1cf4f46add28bb3
nestor <nestorcolt@hotmail.com>
2021-08-05 15:58:51 +0200

    api error to web fixed

--- packages/requests/request_manager.py
+++ packages/requests/request_manager.py
@@ -78,14 +78,8 @@ def send_block_to_web(user_id, block_data, **kwargs):
 
 @authorize_request
 def send_error_to_web(user_id, **kwargs):
-
-    print(constants.WEB_BACKEND_ERROR_ENDPOINT_URL)
-    print(json.dumps({"user_id": user_id}))
-    headers = kwargs["access_token"]
-    print(headers)
-
     response = requests.post(constants.WEB_BACKEND_ERROR_ENDPOINT_URL,
-                             json=json.dumps({"user_id": user_id}),
+                             json={"user_id": user_id},
                              headers=kwargs["access_token"])
 
     return response.status_code
